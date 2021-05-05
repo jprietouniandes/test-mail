6f5c8715c7c97fa5e8c43a740db045f3068512a8
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2021-05-05 16:19:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,7 @@ class RecursoListarPublicaciones(Resource):
             })
 
     def get(self):
+        send_simple_message()
         publicaciones = Publicacion.query.all()
         return posts_schema.dump(publicaciones)
     
