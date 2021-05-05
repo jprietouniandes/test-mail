fbe9fff2f3da89beca7ffcb0a8ca140ba6fed6e3
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2021-05-05 16:42:05 -0500

    Mail changes send

--- app.py
+++ app.py
@@ -29,18 +29,15 @@ posts_schema = Publicacion_Schema(many = True)
 
 class RecursoListarPublicaciones(Resource):
     def get(self):
-        return requests.post('https://api.mailgun.net/v3/' + domain_name + '/messages'
+        requests.post('https://api.mailgun.net/v3/' + domain_name + '/messages'
                              , auth=('api', api_key), data={
             'from': 'Excited User <mailgun@sandboxc8505dca72c843bb9d4b5e8e78af30b1.mailgun.org>',
             'to': ['jmauricio_1107@hotmail.com'],
             'subject': 'Hello',
             'text': 'Testing some Mailgun awesomness!',
             })
-
-    #def get(self):
-        #send_simple_message()
-        #publicaciones = Publicacion.query.all()
-        #return posts_schema.dump(publicaciones)
+        publicaciones = Publicacion.query.all()
+        return posts_schema.dump(publicaciones)
     
     def post(self):
             nueva_publicacion = Publicacion(
