dcc83af9395b484e5869bc6f2356387e68d4df0c
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2021-05-05 16:10:34 -0500

    ident

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ posts_schema = Publicacion_Schema(many = True)
 
 class RecursoListarPublicaciones(Resource):
    def send_simple_message():
-    	return requests.post('https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages'
-                             , auth=('api', 'YOUR_API_KEY'), data={
-            'from': 'Excited User <mailgun@YOUR_DOMAIN_NAME>',
-            'to': ['bar@example.com', 'YOU@YOUR_DOMAIN_NAME'],
-            'subject': 'Hello',
-            'text': 'Testing some Mailgun awesomness!',
-            })
+       return requests.post('https://api.mailgun.net/v3/' + domain_name + '/messages'
+                            , auth=('api', api_key), data={
+           'from': 'Excited User <mailgun@sandboxc8505dca72c843bb9d4b5e8e78af30b1.mailgun.org>',
+           'to': ['jmauricio_1107@hotmail.com'],
+           'subject': 'Hello',
+           'text': 'Testing some Mailgun awesomness!',
+           })
 
     def get(self):
 		send_simple_message()
