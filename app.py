39aecb35976bad63f0a443be86cdeb8b9eaa1ccc
jprietouniandes <47231655+jprietouniandes@users.noreply.github.com>
2021-05-05 15:11:47 -0500

    test mail

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
-api_key = os.environ.get('API_KEY')
+api_key = os.environ.get('MAILGUN_API_KEY')
 
 class Publicacion(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -67,7 +67,7 @@ def send_simple_message():
 	return requests.post(
 		"https://api.mailgun.net/v3/sandboxc8505dca72c843bb9d4b5e8e78af30b1/messages",
 		auth=("api", api_key),
-		data={"from": "Excited User <mailgun@sandboxc8505dca72c843bb9d4b5e8e78af30b1>",
+		data={"from": "Excited User <mailgun@sandboxc8505dca72c843bb9d4b5e8e78af30b1.mailgun.org>",
 			"to": ["jmauricio_1107@hotmail.com"],
 			"subject": "Hello",
 			"text": "Testing some Mailgun awesomness!"})
